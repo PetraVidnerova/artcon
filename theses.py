5c9d7a10db01b7e5ac8356feba6f07e7519722e7
Petra Vidnerova <petra.vidnerova@gmail.com>
2026-03-13 12:56:00 +0100

    theses

---
+++ theses.py
@@ -0,0 +1,16 @@
+THESES = [
+    # Obecné teze pro
+    "Artificial consciousness is currently possible.",
+    "Artificial consciousness will be possible in the future.",
+    # Obecné teze proti
+    "Currently, artificial consciousness is not possible.",
+    "In principle, artificial consciousness is not possible.",
+    # Specifické teze
+    "Functionalism about consciousness is correct.",
+    "Functionalism about consciousness is false.",
+    # Ještě specifičtější teze
+    "Computational functionalism about consciousness is correct.",
+    "Computational functionalism about consciousness is false.",
+    "Biology is necessary for consciousness.",
+    "Biology is not necessary for consciousness.",
+]
